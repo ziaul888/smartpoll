64bb278ea539a98a9482826d04abc5a2da54d6fb
unknown <ziaul935@gmail.com>
2020-11-03 01:54:47 +0600

    add delete selectpoll

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
 import logo from './logo.svg';
+import shortid from 'shortid'
 import './App.css';
+import MainContent from './components/main-content'
+import SideBar from './components/sidebar'
+import React, { Component } from 'react'
+import { Container,Row,Col} from 'reactstrap'
+import POLLS from './data/polls'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+   state={
+     polls:[],
+     selectedPoll:{},
+     searchTerm:""
+
+   }
+
+   componentDidMount(){
+     this.setState({polls:POLLS})
+   }
+ addNewPoll= poll =>{
+ poll.id = shortid.generate(),
+ poll.created = new Date()
+ poll.totalVote = 0
+ poll.opinions=[]
+
+ this.setState({
+   polls:this.state.polls.concat(poll)
+ })
+
 }
+ 
+updatePoll =updatedPoll =>{
+ const polls =[...this.state.polls]
+ const poll = polls.find(p=> p.id === updatedPoll.id)
+ poll.title = updatedPoll.title
+ poll.descrption= updatedPoll.descrption
+ poll.opinions = updatedPoll.opinions
+}
+  deletePoll =pollId=> {
+    const polls = this.state.polls.filter(p=> p.id !==pollId)
+    this.setState({polls, selectedPoll:{}})
+  }
+  selectedPoll=pollId=>{
+    const poll= this.state.polls.find(p=>p.id ===pollId)
+    this.setState({
+      selectedPoll:poll
+    })
 
-export default App;
+  }
+
+  render() {
+    return (
+         <Container className="my-5">
+           <Row>
+             <Col md={4}>
+               <SideBar/>
+             </Col>
+             <Col md={8}>
+               <MainContent/>
+             </Col>
+           </Row>
+         </Container>
+    )
+  }
+}
